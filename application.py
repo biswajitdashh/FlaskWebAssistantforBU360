436696f31d236b1caa47ccda9c5d311cf81c27e6
Biswajit Dash <biswajitdash404@gmail.com>
2024-06-02 12:40:43 +0530

    initial commit

---
+++ application.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+application = Flask(__name__)
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+@application.route('/', methods=['GET','POST'])
+def index():
+    res=""
+    if request.method == 'POST':
+            system_behavior=request.form["system_behavior"]
+            data=request.form["data"]
+            response = openai.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content":system_behavior},
+                    {"role": "user", "content": data},
+                    {"role": "assistant", "content":"Refer to the inputs given by the user."},
+                ])
+            res=response.choices[0].message.content
+    return render_template("index.html",result=res)
+if __name__ == '__main__':
+    application.run(debug=True) 
